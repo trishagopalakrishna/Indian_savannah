786eae09386f650a1367202734b54ec8dffa54b5
Trisha Gopalakrishna <44071773+trishagopalakrishna@users.noreply.github.com>
2021-02-16 16:28:39 +0000

    Added conversion factor from guidelines to prepare sand data

--- Data_Prep/soil_prep.R
+++ Data_Prep/soil_prep.R
@@ -26,14 +26,19 @@ sand15_30<-raster("C://Users//Trisha_Gopalakrishna//OneDrive - Nexus365//Paper2/
 sand0_5 
 #Projection is Homolosine projection (equal area)- https://www.isric.org/explore/soilgrids/faq-soilgrids
 
+#All rasters need to be divided by conversion factor 10- https://www.isric.org/explore/soilgrids/faq-soilgrids
+sand0_5_converted<-sand0_5/10
+sand5_15_converted<-sand5_15/10
+sand15_30_converted<-sand15_30/10
+
 #Changing projection to WGS 1984
 
 mcwd<-raster("C://Users//Trisha_Gopalakrishna//OneDrive - Nexus365//Paper2//Data//Rainfall//MCWD//mcwd.tif")
 mcwd #weird resolution 0.04165702 ~4km - SPATIAL RESOLUTION OF ANALYSIS
 
-sand0_5_proj<-projectRaster(sand0_5, mcwd, method = 'bilinear')
-sand5_15_proj<-projectRaster(sand5_15,mcwd, method = 'bilinear')
-sand15_30_proj<-projectRaster(sand15_30, mcwd, method = 'bilinear')
+sand0_5_proj<-projectRaster(sand0_5_converted, mcwd, method = 'bilinear')
+sand5_15_proj<-projectRaster(sand5_15_converted,mcwd, method = 'bilinear')
+sand15_30_proj<-projectRaster(sand15_30_converted, mcwd, method = 'bilinear')
 
 writeRaster(sand0_5_proj,"C://Users//Trisha_Gopalakrishna//OneDrive - Nexus365//Paper2//Data//Soil//Sand//sand0_5_proj.tif")
 writeRaster(sand5_15_proj,"C://Users//Trisha_Gopalakrishna//OneDrive - Nexus365//Paper2//Data//Soil//Sand//sand5_15_proj.tif")
